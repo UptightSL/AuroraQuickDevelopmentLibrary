2d257cf46b37885046f31d915df45358c5e2e9af
Uptight <72242372+UptightSL@users.noreply.github.com>
2021-11-15 12:27:14 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,14 @@
-
+scriptTitle = "QDL"
+scriptAuthor = "UptightSL"
+scriptVersion = 1
+scriptDescription = "This script is just for testing :)"
+--[[
+   ____  _____  _      
+  / __ \|  __ \| |     
+ | |  | | |  | | |     
+ | |  | | |  | | |     
+ | |__| | |__| | |____ 
+  \___\_\_____/|______|
+                       
+            By UptightSL
+]]--
